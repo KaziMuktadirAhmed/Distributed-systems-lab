b86db93bddf5b6aea6dc40d75988d8835adf0a41
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-06-27 14:08:29 +0600

    from class

--- JS practice/practice.js
+++ JS practice/practice.js
@@ -1,5 +1,11 @@
 console.log('hello world!!!!\njalo jalo agun jalo');
 // window.print();  eta local js script e kaj korbe na karon kono running html page nai
 a = 'kazi';
-b = 'ekta baal';
-console.log(a + b);
+b = 'ekta baal ';
+var num1 = 23; num2 = 45;
+var n1 = 4, n2 = 3, n3 = 4, n4 = 8;
+var h;
+h = 32;
+console.log(a + " " + b + (num1 + num2) + " " + (n1 + n4) + "\nhello : " + h);
+let x = "5" + 2 + 3;
+console.log(x);
