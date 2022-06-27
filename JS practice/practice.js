e2823395bdf5a1dd4367cb68ad681ca77428a572
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-06-27 12:03:30 +0600

    from class

--- JS practice/practice.js
+++ JS practice/practice.js
@@ -1,1 +1,5 @@
 console.log('hello world!!!!\njalo jalo agun jalo');
+// window.print();  eta local js script e kaj korbe na karon kono running html page nai
+a = 'kazi';
+b = 'ekta baal';
+console.log(a + b);
