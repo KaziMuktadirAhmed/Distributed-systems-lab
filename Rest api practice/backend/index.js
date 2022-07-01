27af54d7b5ae41aa639239b2f58aed6bdae955e9
KaziMuktadir <kazisujoy@gmail.com>
2022-07-02 03:05:55 +0600

    connected database

---
+++ Rest api practice/backend/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./db.js");
+
+const app = express();
+
+// DB connection
+connectDB();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+//
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('llistening on port ' + port + ' ...'));
