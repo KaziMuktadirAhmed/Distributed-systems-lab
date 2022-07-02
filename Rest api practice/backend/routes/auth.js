0557bcc04e456a979b5bfbd61c64c4dcb89eda25
KaziMuktadir <kazisujoy@gmail.com>
2022-07-03 04:50:14 +0600

    final commit of the day (most likely)

--- Rest api practice/backend/routes/auth.js
+++ Rest api practice/backend/routes/auth.js
@@ -10,11 +10,11 @@ router.post("/", async (req, res) => {
         if(error)
             return res.status(400).send({message: error.details[0].message});
 
-        const user = User.findOne({email: req.body.email});
-        
+        const user = await User.findOne({email: req.body.email});
         if(!user)
             return res.status(401).send({message: "Invalid email or password !!"});
         
+        console.log(req.body.email);
         console.log(user);
         const validPassword = bcrypt.compare(req.body.password, user.password);
         if(!validPassword)
