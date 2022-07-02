bc449dc52338029ce5bf596306e25f53da596506
KaziMuktadir <kazisujoy@gmail.com>
2022-07-03 00:25:06 +0600

    added routing and post method for new user

---
+++ Rest api practice/backend/routes/users.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const {User, validate} = require('../models/user.js');
+
+router.post("/", async (req, res) => {
+    try {
+        const {error} = validate(req.body);
+
+        if(error) 
+            return res.status(400).send({messege: error.details[0].messege});
+
+        const user = await User.findOne({email: req.body.email});
+
+        if(user)
+            return res.status(409).send({messege: "User with given email alread exists !!!"});
+        
+        // generate salt and password hash for storage
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        // storing user data and password hash in db
+        await new User({ ...req.body, password: hashPassword }).save();
+        res.status(201).send({ message: "User created successfully" });
+        
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error !!!" });
+    }
+});
+
+module.exports = router;
