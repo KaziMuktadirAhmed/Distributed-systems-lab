970e5ddbc257426a584e8601b478c7bbc0920a6d
KaziMuktadir <kazisujoy@gmail.com>
2022-07-09 14:27:24 +0600

    push before test

--- Rest api practice/backend/index.js
+++ Rest api practice/backend/index.js
@@ -10,6 +10,7 @@ const connectDB = require("./db.js");
 //import routes 
 const usersRoute = require('./routes/users.js');
 const authRoute = require('./routes/auth.js');
+const postRoute = require('./routes/posts.js');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(cors());
 // routes
 app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/post', postRoute);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('llistening on port ' + port + ' ...'));
--- Rest api practice/backend/routes/posts.js
+++ Rest api practice/backend/routes/posts.js
@@ -3,8 +3,7 @@ const Joi = require('joi');
 const {Post} = require('../models/post.js');
 const verifyUser = require('../utils/verify.js');
 
-router.post("/", async (req, res) => {
-    
+router.post("/", (req, res) => {
     try {
         const {error} = validate(req.body);
         if(error)
@@ -13,12 +12,12 @@ router.post("/", async (req, res) => {
         const hasValidToken = verifyUser.verifyUser(req.body.jwtToken);
         
         if(hasValidToken){
-            await new Post({
+            new Post({
                 fullName: req.body.fullName,
                 message: req.body.message,
                 date: req.body.date,
             }).save((err, doc) => {
-                if(err) res.status(402).send({ message: "Error at saving post data"});
+                if(err) res.status(402).send({ message: "Error at saving post data !!!"});
                 else res.status(200).send({ message: "Post saved successfully ..." });
             });
         }
@@ -32,8 +31,8 @@ router.post("/", async (req, res) => {
 
 const validate = (data) => {
 	const schema = Joi.object({
-		fullName: Joi.string().required().label("Email"),
-		message: Joi.string().label("Password"),
+		fullName: Joi.string().required().label("Full name"),
+		message: Joi.string().label("Message"),
         date: Joi.string().required().label("Date"),
         jwtToken: Joi.string().required().label("Jwt-token"),
 	});
