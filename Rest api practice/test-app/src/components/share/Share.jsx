be9ed08c736fa019344b148f201498ff6af1d747
KaziMuktadir <kazisujoy@gmail.com>
2022-07-22 01:33:35 +0600

    added ui for posting story or status

--- Rest api practice/test-app/src/components/share/Share.jsx
+++ Rest api practice/test-app/src/components/share/Share.jsx
@@ -1,10 +1,41 @@
 import './Share.css'
 
+import {PermMedia, Label, Room, EmojiEmotions} from '@material-ui/icons'
+
 export default function Share() {
   return (
-    <div className='share'>
-      <div className="shareTop"></div>
-      <div className="shareBottom"></div>
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+          <input
+            placeholder="What's in your mind ?"
+            className="shareInput"
+          />
+        </div>
+        <hr className="shareHr"/>
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <PermMedia htmlColor="tomato" className="shareIcon"/>
+                    <span className="shareOptionText">Photo or Video</span>
+                </div>
+                <div className="shareOption">
+                    <Label htmlColor="blue" className="shareIcon"/>
+                    <span className="shareOptionText">Tag</span>
+                </div>
+                <div className="shareOption">
+                    <Room htmlColor="green" className="shareIcon"/>
+                    <span className="shareOptionText">Location</span>
+                </div>
+                <div className="shareOption">
+                    <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                    <span className="shareOptionText">Feelings</span>
+                </div>
+            </div>
+            <button className="shareButton">Share</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
