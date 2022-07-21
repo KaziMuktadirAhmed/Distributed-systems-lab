c3839e674c9b27691eee48afb3041ff612e36ad4
KaziMuktadir <kazisujoy@gmail.com>
2022-07-22 02:09:09 +0600

    last commit for the day mathabetha kortese prochur

--- Rest api practice/test-app/src/components/feed/Feed.jsx
+++ Rest api practice/test-app/src/components/feed/Feed.jsx
@@ -4,21 +4,21 @@ import Share from "../share/Share";
 import "./Feed.css";
 
 export default function Feed() {
-  const srcList = [
-    "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-    "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-  ];
+  // const srcList = [
+  //   "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
+  //   "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
+  // ];
 
 
 
   return (
     <div className="feed">
       <Share />
-      {srcList.map((src) => {
+      {/* {srcList.map((src) => {
         return <img src={src} width="650px" height="500px" alt="wraith waifu" style={{
           margin: '20px'
         }}/>;
-      })}
+      })} */}
 
     </div>
   );
