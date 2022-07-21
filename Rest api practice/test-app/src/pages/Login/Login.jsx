305f73f5ab6960ec6d86e16a6ed553fcba4360c1
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-07-21 14:17:25 +0600

    connected login with backend

--- Rest api practice/test-app/src/pages/Login/Login.jsx
+++ Rest api practice/test-app/src/pages/Login/Login.jsx
@@ -1,27 +1,53 @@
-import './Login.css'
+import { useRef } from "react";
+import "./Login.css";
 
 export default function Login() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSubmit = () => {
+    fetch("http://localhost:4000/api/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      }),
+    }).then(response => response.json()).then(
+        obj => console.log(obj)
+    );
+  };
+
   return (
     <div className="login">
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className="loginLogo">Zucccbook</h3>
-                <span className="loginDesc">
-                    Connect with someone and do the thing
-                </span>
-            </div>
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder='Email' className="loginInput" />
-                    <input placeholder='Password' className="loginInput" />
-                    <button className="loginButton">Log in</button>
-                    <span className="loginForgot">Forgot password ?</span>
-                    <button className="loginRegisterButton">
-                        Create a new Account
-                    </button>
-                </div>
-            </div>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Zucccbook</h3>
+          <span className="loginDesc">
+            Connect with someone and do the thing
+          </span>
         </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input ref={emailRef} placeholder="Email" className="loginInput" />
+            <input
+              ref={passwordRef}
+              placeholder="Password"
+              className="loginInput"
+              type="password"
+            />
+            <button className="loginButton" onClick={handleSubmit}>
+              Log in
+            </button>
+            <span className="loginForgot">Forgot password ?</span>
+            <button className="loginRegisterButton">
+              Create a new Account
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
