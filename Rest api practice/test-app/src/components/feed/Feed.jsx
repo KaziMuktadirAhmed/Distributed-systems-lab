70bad354eccab3a7a9a8d2be7f4ea47aba759bd1
KaziMuktadir <kazisujoy@gmail.com>
2022-07-23 16:13:51 +0600

    post status done

--- Rest api practice/test-app/src/components/feed/Feed.jsx
+++ Rest api practice/test-app/src/components/feed/Feed.jsx
@@ -4,14 +4,9 @@ import "./Feed.css";
 import Post from "../post/Post";
 import Share from "../share/Share";
 
-
 import { useState } from "react";
 
 export default function Feed() {
-  // const srcList = [
-  //   "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-  //   "https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-1520x800.jpeg",
-  // ];
   const [posts, setPosts] = useState([]);
 
   const getPosts = () => {
@@ -23,7 +18,6 @@ export default function Feed() {
       },
     }).then(response => response.json()).then(
         obj =>{ 
-          console.log(obj);
           setPosts(obj);
         }
     );
