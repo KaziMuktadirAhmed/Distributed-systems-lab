81bd1bf5263a3b1489db7b78ae9bad9eef91f1de
KaziMuktadir <kazisujoy@gmail.com>
2022-07-24 03:24:39 +0600

    did the thing

--- Rest api practice/test-app/src/components/add-story/AddStory.jsx
+++ Rest api practice/test-app/src/components/add-story/AddStory.jsx
@@ -3,15 +3,13 @@ import './AddStory.css';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 function AddStory({ profileSrc }) {
-    const handelAdd = () => {
-        console.log('added story');
-};
-
   return (
     <div className='addstory' style={{backgroundImage:`url(${profileSrc})`}}>
         <form action="/profile" method="post" enctype="multipart/form-data">
             <input type="file" name="avatar" />
-            <button type='submit'>Add story</button>
+            <button type='submit'>
+                <AddCircleIcon/>
+            </button>
         </form>
         <h4>Add Story</h4>
     </div>
