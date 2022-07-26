51e1ce35e1b1a219d339296858b27f8968834226
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-07-26 14:16:12 +0600

    seeeessssh

--- Rest api practice/backend/utils/verify.js
+++ Rest api practice/backend/utils/verify.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 function verifyUser (req, res, next) {
     if (req.cookies){
-        console.log(req.cookies);
         jwt.verify(req.cookies.jwt, process.env.JWT_PVT_KEY, (err, userInfo) => {
             if(err){  
                 res.status(500).send("Error in verifying jwt token");
